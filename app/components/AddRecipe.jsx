372113eb7622b00ea7aa838ea3c83200d2742799
Nicholas Pritchyk <nickpritchyk20@gmail.com>
2023-07-12 12:49:45 -0600

    form validation, img wip

--- app/components/AddRecipe.jsx
+++ app/components/AddRecipe.jsx
@@ -51,8 +51,8 @@ function AddRecipe() {
 
   const sendRecipe = async (e) => {
     e.preventDefault();
-    setIsLoading(true)
-    console.log(ingredientsArr)
+    if(title && ingredientsArr && instructions && description && cookTime) {
+      setIsLoading(true)
     const ingredientsJSON = JSON.stringify(ingredientsArr)
     await fetch('/api/main', {
       method: 'POST',
@@ -71,40 +71,42 @@ function AddRecipe() {
       setIsLoading(false)
       handleSuccess()
       })
-    }
+    } else {
+      alert('Please fill out all input fields.')
+    }}
     
   return (
     <div className="flex flex-col w-full items-center">
       <h1 className='text-2xl font-bold mt-4'> Add Recipe </h1>
       <form className='bg-white h-max w-[75%] sm:w-[35%] flex m-12 flex-col space-y-6 rounded-lg'>
         <label> Recipe Title </label>
-        <input className='border-[0.5px] border-black p-1 shadow-sm' placeholder='Title' onChange={(e) => setTitle(e.target.value)}></input>
+        <input className='border-[0.5px] border-black p-1 shadow-sm' placeholder='Title' onChange={(e) => setTitle(e.target.value)} required></input>
         <label> Description </label>
-        <input className='border-[0.5px] border-black p-1 shadow-sm' placeholder='Describe your recipe' onChange={(e) => setDescription(e.target.value)}></input>
+        <input className='border-[0.5px] border-black p-1 shadow-sm' placeholder='Describe your recipe' onChange={(e) => setDescription(e.target.value)} required></input>
         <label> Cook Time (in minutes) </label>
-        <input className='border-[0.5px] border-black p-1 shadow-sm' placeholder='Total time' type='number' onChange={(e) => setCookTime(e.target.value)}></input>
+        <input className='border-[0.5px] border-black p-1 shadow-sm' placeholder='Total time' type='number' onChange={(e) => setCookTime(e.target.value)} required></input>
         <label> Ingredients </label>
         <div className='flex relative items-center'>
           <input className='border-[0.5px] border-black p-1 shadow-sm w-full' placeholder='Add one item at a time' value={ingredients} onChange={(e) => setIngredients(e.target.value)}></input>
-          <button className='flex bg-blue-500 rounded-md absolute right-1' type='button' onClick={handleIngredientClick}> <AddIcon /> </button>
+          <button className='flex bg-blue-500 rounded-md absolute right-1' type='button' onClick={handleIngredientClick}><AddIcon /></button>
         </div>
         {ingredientsArr.length > 0 &&
         <ul className='sm:text-[18px] border-y-[1px] p-4'>
           {ingredientsArr.map((res) => 
             <div className='flex'>
-              <button type='button' className='hover:scale-[1.2] px-2'> <ClearIcon style={{color: '#C0C0C0', fontSize: '16px'}} onClick={() => handleDeleteIngredient(res)} /> </button>
+              <button type='button' className='hover:scale-[1.2] px-2'> <ClearIcon style={{color: '#C0C0C0', fontSize: '16px'}} onClick={() => handleDeleteIngredient(res)} /></button>
               <li className=''> {res} </li>
             </div>
           )}
         </ul>
         }
         <label> Instructions </label>
-        <ReactQuill className='' type='button' placeholder='Instructions' value={instructions} onChange={setInstructions}></ReactQuill> 
+        <ReactQuill className='' type='button' placeholder='Instructions' value={instructions} onChange={setInstructions} required></ReactQuill> 
         <label> Image </label>
         <AddAPhotoIcon />
         <div className='flex mx-auto h-full'>
         {!isLoading &&
-          <Button className='h-[2rem]' onClick={sendRecipe}> Submit Recipe </Button>
+          <Button type='submit' className='h-[2rem]' onClick={sendRecipe}> Submit Recipe </Button>
         }{isLoading && 
           <Bounce className='h-[2rem]' />
         }
