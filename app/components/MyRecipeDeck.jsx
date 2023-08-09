abd93cacb353733725f96976e3e043ca77856b20
Nicholas Pritchyk <nickpritchyk20@gmail.com>
2023-08-08 18:02:11 -0600

    breakpoints for myrecipe deck

--- app/components/MyRecipeDeck.jsx
+++ app/components/MyRecipeDeck.jsx
@@ -6,10 +6,10 @@ import AddCommentOutlinedIcon from '@mui/icons-material/AddCommentOutlined';
 export default function MyRecipeDeck({ recipes }) {
 
     return (
-        <div className="grid grid-cols-4 gap-4 justify-items-center m-12"> 
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 m-12"> 
         {recipes.map((res, key) => 
             
-            <div key={res.id} className="relative w-full h-fit mb-12 border border-gray-200 rounded-lg shadow-lg bg-white hover:scale-[1.01]">
+            <div key={res.id} className="flex w-96 sm:w-82 md:w-64 lg:w-82 h-fit mb-12 border border-gray-200 rounded-lg shadow-lg bg-white hover:scale-[1.01]">
                 <img className="object-fill" src={res.imgurl} alt='img' />
                 <div className="p-5 left-0">
                     <h5 className="mb-2 text-2xl font-bold tracking-tight bottom">{res.name}</h5>
